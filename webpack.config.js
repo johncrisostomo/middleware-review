0c6fa6b0d233c827d6e7e3c157fc2a8e9d991443
John Crisostomo <jd@snapzio.com>
2017-07-11 09:21:55 +0800

    rolls back babel presets for object spread

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['react', 'env'],
+          presets: ['react', 'es2015', 'stage-1'],
         },
       },
     ],
